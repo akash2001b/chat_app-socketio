38bc8cc656df6514a673e738b3ae781d9d080ce9
akash Roy Choudhury <arc.ar602@gmail.com>
2021-07-21 03:02:31 +0530

    socket.io setup done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 const express=require('express');
+const http=require('http');
 const path=require('path');
+const socketio=require('socket.io');
+const cors=require('cors');
 
 const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
 
 const port=process.env.PORT || 3000;
 
 const publicDirectoryPath=path.join(__dirname,'../public');
 
+app.use(cors());
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, ()=>{
+io.on('connection',(socket)=>{
+    console.log('New websocket connection');
+
+    socket.emit('message','Welcome!');
+
+
+    socket.on('sendMessage',(message)=>{
+
+        io.emit('message',message);
+    });
+
+    
+});
+
+
+
+
+server.listen(port, ()=>{
     console.log(`Sever is up on port ${port}`);
 });
