1ef1d5a745c77c99336d04120d045f47bab24d75
akash Roy Choudhury <arc.ar602@gmail.com>
2021-07-20 22:37:34 +0530

    chat-app setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const path=require('path');
+
+const app=express();
+
+const port=process.env.PORT || 3000;
+
+const publicDirectoryPath=path.join(__dirname,'../public');
+
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, ()=>{
+    console.log(`Sever is up on port ${port}`);
+});
